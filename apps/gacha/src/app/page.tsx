41a08420263cd3aa27b84884750e6ae314eec035
boboiazumi <boboi0704@gmail.com>
2025-03-16 22:18:48 +0700

    feat: modals-gacha

--- apps/gacha/src/app/page.tsx
+++ apps/gacha/src/app/page.tsx
@@ -1,11 +1,29 @@
+import { Button } from '@imphnen-frontend-service/ui/atoms';
 import { LandingPage } from '@imphnen-frontend-service/ui/organisms';
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
+
+import { createPortal } from 'react-dom';
+import { ModalsGacha } from "@imphnen-frontend-service/ui/organisms"
 
 export const Components: FC = (): ReactElement => {
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <>
       <LandingPage />
-      <div></div>
+
+      {/**Izin pake untuk debug modals gacha */}
+      <div>
+        <p>Debug : Test Modals Gacha</p>
+        <Button onClick={
+          () => setShowModal(true)
+        }>Tekan</Button>
+      </div>
+      {showModal && createPortal(
+        <ModalsGacha></ModalsGacha>,
+        document.body
+      )}
+      {/**Izin pake untuk debug modals gacha */}
     </>
   );
 };
--- libs/ui/src/organisms/index.ts
+++ libs/ui/src/organisms/index.ts
@@ -1,2 +1,3 @@
 export * from './navbar';
 export * from './landing-page';
+export * from "./modals-gacha";
