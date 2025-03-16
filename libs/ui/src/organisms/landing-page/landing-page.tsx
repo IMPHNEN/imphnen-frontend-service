02e06caca10df81e2861c790960d24eddf047704
Hafid Nur <hafidnurazis@gmail.com>
2025-03-16 07:41:16 +0700

    feat: landing page for desktop viewport

--- libs/ui/src/organisms/landing-page/landing-page.tsx
+++ libs/ui/src/organisms/landing-page/landing-page.tsx
@@ -13,9 +13,9 @@ export const LandingPage: FC = (): ReactElement => {
   return (
     <section
       id="landing-page"
-      className="my-12 md:my-24 flex flex-col md:flex-row flex-wrap"
+      className="my-12 md:my-24 flex flex-col md:flex-row flex-wrap max-w-[1280px] items-center justify-center lg:mx-auto"
     >
-      <div className="order-none lg:order-1 flex flex-col md:w-full items-center gap-7 md:gap-13 text-primary-500 mb-10 md:mb-20">
+      <div className="order-none lg:order-1 flex flex-col xl:flex-2 md:w-full lg:w-fit items-center gap-7 md:gap-13 text-primary-500 mb-10 md:mb-20">
         <img
           src="/logos/logo.svg"
           alt="IMPHNEN Logo"
@@ -23,7 +23,7 @@ export const LandingPage: FC = (): ReactElement => {
         />
 
         <div className="font-semibold text-center">
-          <div className="text-p1 md:text-h2 text-center">
+          <div className="text-p1 md:text-h2 lg:font-bold text-center">
             <p>New Merchandise</p>
             <p>Coming UP</p>
           </div>
@@ -39,7 +39,7 @@ export const LandingPage: FC = (): ReactElement => {
         <Button
           size="sm"
           variant="bordered"
-          className="flex gap-3 px-3 py-2 bg-primary-100 border border-primary-500 border-dashed rounded animate-bounce text-label2 md:text-p2"
+          className="flex gap-3 px-3 md:px-5 py-2 md:py-4 bg-primary-100 border border-primary-500 border-dashed rounded md:rounded-md animate-bounce text-label2 md:text-p2"
           onClick={scrollToRoulette}
         >
           <ArrowDownOutlined />
@@ -47,8 +47,9 @@ export const LandingPage: FC = (): ReactElement => {
           <ArrowDownOutlined />
         </Button>
       </div>
-      <div className="order-1 flex flex-col flex-1 justify-center items-center mb-8">
-        <div className="relative h-[150px] md:h-[250px]">
+
+      <div className="order-1 lg:order-none flex flex-col flex-1 justify-center items-center mb-8 relative">
+        <div className="relative h-[150px] md:h-[230px]">
           <img
             src="/merch/1.png"
             alt="Merch 1"
@@ -66,16 +67,21 @@ export const LandingPage: FC = (): ReactElement => {
         <div className="mt-2 text-primary-500 font-bold text-p2 md:text-h3">
           ~ 175k ~
         </div>
+        <img
+          className="absolute bottom-12 -right-10"
+          src="/landing-arrow-1.svg"
+          alt=""
+        />
       </div>
-      <div className="order-2 flex flex-col flex-1 justify-center items-center mb-8">
-        <div className="relative h-[130px] md:h-[250px]">
+      <div className="order-2 flex flex-col flex-1 justify-center items-center mb-8 relative">
+        <div className="relative h-[130px] md:h-[230px]">
           <img
             src="/merch/2.png"
             alt="Merch 2"
-            className="relative top-[5px] md:top-[8px] left-[5px] md:left-[12px] z-10 w-[165px] md:w-[277px]"
+            className="relative top-[5px] md:top-[8px] left-0 md:left-[5px] z-10 w-[165px] md:w-[277px]"
           />
           <img
-            className="absolute top-0 left-0 z-0 min-w-[174px] md:min-w-[302px]"
+            className="absolute top-0 -left-[5px] md:-left-[8px] z-0 min-w-[174px] md:min-w-[302px]"
             src="/merch/Vector-2.svg"
             alt=""
           />
@@ -86,6 +92,11 @@ export const LandingPage: FC = (): ReactElement => {
         <div className="mt-2 text-primary-500 font-bold text-p2 md:text-h3">
           ~ 90k ~
         </div>
+        <img
+          className="absolute -bottom-10 -left-12"
+          src="/landing-arrow-2.svg"
+          alt=""
+        />
       </div>
     </section>
   );
