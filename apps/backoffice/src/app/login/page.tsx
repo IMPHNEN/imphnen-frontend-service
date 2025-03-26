90ab974e034e400044d2e0467715b4233075016d
Hafid Nur <hafidnurazis@gmail.com>
2025-03-27 02:42:57 +0700

    feat: backoffice login page

---
+++ apps/backoffice/src/app/login/page.tsx
@@ -0,0 +1,30 @@
+import { FC, ReactElement } from 'react';
+import {
+  Input,
+  Button,
+  PasswordInput,
+} from '@imphnen-frontend-service/ui/atoms';
+
+export const Components: FC = (): ReactElement => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="bg-white border border-primary-200 shadow-lg p-[60px] text-center flex flex-col gap-8 rounded-2xl">
+        <img src="/logos/logo.svg" alt="" className="h-[70px] w-auto" />
+        <h1 className="text-primary-500 text-p1 font-semibold">
+          Welcome to IMPHNEN Backoffice
+        </h1>
+        <div className="flex gap-[8px] flex-col">
+          <div className="self-start text-p3 font-medium">Email</div>
+          <Input placeholder="Masukkan Email" type="email" size="lg" />
+        </div>
+        <div className="flex gap-[8px] flex-col">
+          <div className="self-start text-p3 font-medium">Password</div>
+          <PasswordInput placeholder="Masukkan Password" size="lg" />
+        </div>
+        <Button>Login</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Components;
