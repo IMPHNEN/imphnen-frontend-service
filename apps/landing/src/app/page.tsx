0478d12ffc03ca3aa866aa874e314b09c5885e54
ArraysID <arraysdotid@gmail.com>
2025-04-27 05:44:11 +0700

    feat: migrate community component

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Community } from './_components/community';
 import { Features } from './_components/features';
 import { Header } from './_components/header';
 import { Hero } from './_components/hero';
@@ -9,6 +10,7 @@ export default function Page() {
       <main className="flex-1">
         <Hero />
         <Features />
+        <Community />
       </main>
     </div>
   );
