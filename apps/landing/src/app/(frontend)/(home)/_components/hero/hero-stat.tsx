e5844327a584ffcdf88bda66a598723004a7ad7c
arraysid <arraysdotid@gmail.com>
2025-05-17 10:41:22 +0700

    feat: remove unused images

--- apps/landing/src/app/(frontend)/(home)/_components/hero/hero-stat.tsx
+++ apps/landing/src/app/(frontend)/(home)/_components/hero/hero-stat.tsx
@@ -1,17 +1,20 @@
-import { Avatar, AvatarImage } from '@components/atoms';
+import { Avatar, AvatarFallback, AvatarImage } from '@components/atoms';
 
 export function HeroStat() {
   return (
     <div className="flex items-center rounded-full border border-border bg-background p-1 shadow shadow-black/5 md:w-fit">
       <div className="flex -space-x-4">
         <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/53475078?v=4" />
+          <AvatarImage src="/maulana.webp" />
+          <AvatarFallback />
         </Avatar>
         <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/49753444?v=4" />
+          <AvatarImage src="/rasyid.webp" />
+          <AvatarFallback />
         </Avatar>
         <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/97678571?v=4" />
+          <AvatarImage src="/ega.webp" />
+          <AvatarFallback />
         </Avatar>
       </div>
       <p className="px-2 text-xs text-muted-foreground">
