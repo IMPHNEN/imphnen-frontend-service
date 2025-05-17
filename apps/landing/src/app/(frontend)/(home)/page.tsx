ff65424ff9a763b332ca1a1d2335811d4cd8ba2c
arraysid <arraysdotid@gmail.com>
2025-05-17 15:31:00 +0700

    feat: add CallToAction component to encourage community engagement

--- apps/landing/src/app/(frontend)/(home)/page.tsx
+++ apps/landing/src/app/(frontend)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from './_components/call-to-action';
 import { Communities } from './_components/communites';
 import { Hero } from './_components/hero';
 import { Testimonials } from './_components/testimonials';
@@ -8,6 +9,7 @@ export default async function Page() {
       <Hero />
       <Communities />
       <Testimonials />
+      <CallToAction />
     </>
   );
 }
