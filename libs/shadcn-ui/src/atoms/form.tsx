08b4c39c03ccb43170ecb72871c52b9e6aaf224c
arraysid <arraysdotid@gmail.com>
2025-05-23 01:14:10 +0700

    feat: update form label and message styles & add select component

--- libs/shadcn-ui/src/atoms/form.tsx
+++ libs/shadcn-ui/src/atoms/form.tsx
@@ -96,7 +96,7 @@ function FormLabel({
     <Label
       data-slot="form-label"
       data-error={!!error}
-      className={cn('data-[error=true]:text-destructive', className)}
+      className={cn('data-[error=true]:text-rose-500', className)}
       htmlFor={formItemId}
       {...props}
     />
@@ -147,7 +147,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<'p'>) {
     <p
       data-slot="form-message"
       id={formMessageId}
-      className={cn('text-destructive text-sm', className)}
+      className={cn('text-rose-500 text-xs', className)}
       {...props}
     >
       {body}
