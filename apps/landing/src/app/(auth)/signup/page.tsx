fc3c275feac8f8b10f06976b1a0335305e03d1fc
arraysid <arraysdotid@gmail.com>
2025-05-27 13:00:17 +0700

    feat: add signup page

--- apps/landing/src/app/(auth)/signup/page.tsx
+++ apps/landing/src/app/(auth)/signup/page.tsx
@@ -1,9 +1,23 @@
+import { LogoSimple } from '@/app/_components/logo';
 import { Metadata } from 'next';
+import { SignupForm } from './_components/signup-form';
 
 export const metadata: Metadata = {
-  title: 'IMPHNEN - Signin',
+  title: 'IMPHNEN - Signup',
 };
 
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col items-center gap-4">
+        <LogoSimple />
+
+        <p className="text-muted-foreground text-center text-sm sm:text-base">
+          Buat akunmu sekarang
+        </p>
+      </div>
+
+      <SignupForm />
+    </>
+  );
 }
