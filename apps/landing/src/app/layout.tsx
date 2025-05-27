2280bd6085903a12f19be98c77c13bcdb216f8c3
arraysid <arraysdotid@gmail.com>
2025-05-28 01:39:18 +0700

    feat: enable rich colors for Toaster component in layout

--- apps/landing/src/app/layout.tsx
+++ apps/landing/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <Toaster />
+          <Toaster richColors />
         </Providers>
       </body>
     </html>
