57222b655ca8748c18da2d565e67a2a7c4d13613
euxzy <mhd.rijal203@gmail.com>
2025-06-28 15:42:20 +0700

    feat(landing): slicing what we offer section

---
+++ apps/dimentorin/src/app/(public)/(home)/_components/what-we-offer-section.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@imphnen-frontend-service/ui/atoms";
+import { FC } from "react";
+import { motion } from 'framer-motion'
+import { For } from "../../../_components/logic/for";
+
+const OFFERS: { title: string; description: string; image: string }[] = [
+  { title: "AI-Powered Learning", description: "Level Up Instantly!", image: "/image/what-we-offer/ai-powered-learning.webp" },
+  { title: "1-on-1 Mentoring", description: "Sensei dari Dunia Nyata!", image: "/image/what-we-offer/1-on-1-mentoring.webp" },
+  { title: "Community & Resources", description: "Connect dengan Fellow Devs!", image: "/image/what-we-offer/community.webp" },
+]
+
+export const WhatWeOfferSection: FC = () => {
+  return (
+    <section className="container w-full mx-auto px-8 pb-4 max-w-7xl md:pb-8 lg:px-0 lg:pt-4">
+      <div className="mb-6 flex justify-center lg:mb-10">
+        <Button
+          type="button"
+          size="sm"
+          variant="bordered"
+          className="h-auto px-3 py-2 font-semibold bg-primary-100 md:text-base md:px-5 md:py-2.5 lg:text-2xl lg:px-6 lg:py-3"
+        >
+          Apa yang Kami Tawarkan
+        </Button>
+      </div>
+
+      <div className="grid gap-6 md:grid-cols-3">
+        <For data={OFFERS}>
+          {(offer) => (
+            <motion.div
+              className="relative aspect-[3/2] overflow-hidden rounded-2xl md:aspect-[3/4] lg:aspect-[5/6]"
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              <img src={offer.image} alt={offer.title} className="size-full object-cover" />
+              <div
+                className="absolute bottom-0 inset-x-0 p-4 h-1/2 text-primary-50 bg-gradient-to-t from-primary-500 via-primary-500/80 flex flex-col items-center justify-end gap-y-1 md:items-start lg:px-6 lg:pb-8"
+              >
+                <p className="font-semibold lg:text-[26px] lg:font-bold">{offer.title}</p>
+                <p className="text-xs lg:text-[22px] lg:font-medium">{offer.description}</p>
+              </div>
+            </motion.div>
+          )}
+        </For>
+      </div>
+    </section>
+  )
+}
--- apps/dimentorin/src/app/(public)/(home)/page.tsx
+++ apps/dimentorin/src/app/(public)/(home)/page.tsx
@@ -1,9 +1,13 @@
 import { FC, ReactElement } from 'react';
 import { HeroSection } from './_components/hero-section';
+import { WhatWeOfferSection } from './_components/what-we-offer-section';
 
 export const Components: FC = (): ReactElement => {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <WhatWeOfferSection />
+    </>
   );
 };
 
