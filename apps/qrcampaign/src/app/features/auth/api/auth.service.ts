ce8cb8188c6b6df163015935d4c3c0821c3d5d92
Hafid Nur <hafidnurazis@gmail.com>
2026-02-14 18:13:28 +0700

    fix(qr): hardcode API url

--- apps/qrcampaign/src/app/features/auth/api/auth.service.ts
+++ apps/qrcampaign/src/app/features/auth/api/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Define the base URL for the API
-const API_URL = import.meta.env.VITE_API_URL || 'http://api-qr.imphnen.dev/api/v1';
+const API_URL = 'http://api-qr.imphnen.dev/api/v1';
 
 // Create a configured axios instance
 export const api = axios.create({
